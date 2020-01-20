3cca6436d61760c14ec20a7a5bdf43c10679f379
Lunagua <49422409+Lunagua@users.noreply.github.com>
2020-01-20 09:18:40 +0800

    com

---
+++ tuozhong/src/main/java/com/fancha/tuozhong/TuozhongApplication.java
@@ -0,0 +1,13 @@
+package com.fancha.tuozhong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuozhongApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TuozhongApplication.class, args);
+    }
+
+}
